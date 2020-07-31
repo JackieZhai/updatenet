eec0ff22c0bbac7eb723636a3e1aeded99d36dc8
JackieZhai <jackieturing@gmail.com>
2020-07-31 15:47:51 +0800

    a

--- train_updateNet.py
+++ train_updateNet.py
@@ -20,7 +20,7 @@ parser.add_argument('--epochs', default=50, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--print-freq', '-p', default=200, type=int,
                     metavar='N', help='print frequency (default: 10)')
-parser.add_argument('-j', '--workers', default=8, type=int, metavar='N',
+parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 8)')
 parser.add_argument('-b', '--batch-size', default=64, type=int,
                     metavar='N', help='mini-batch size (default: 32)')
