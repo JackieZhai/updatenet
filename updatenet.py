e76c02fdbf856dd8d1f8ee015af4b5ea3c6da144
JackieZhai <jackieturing@gmail.com>
2020-07-31 18:54:26 +0800

    a

--- updatenet.py
+++ updatenet.py
@@ -9,25 +9,25 @@ class UpdateResNet(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(192, 256, 1),
         )
-        self.update2 = nn.Sequential(
-            nn.Conv2d(512, 96, 1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(96, 256, 1),
-        )
-        self.update3 = nn.Sequential(
-            nn.Conv2d(512, 96, 1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(96, 256, 1),
-        )
+        # self.update2 = nn.Sequential(
+        #     nn.Conv2d(512, 96, 1),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(96, 256, 1),
+        # )
+        # self.update3 = nn.Sequential(
+        #     nn.Conv2d(512, 96, 1),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(96, 256, 1),
+        # )
     def forward(self, x, x0):
         #t = torch.cat((x, y, z), 0)
         # x0 is residual
         response = self.update1(x)
-        x1 = x[:,256:512,:,:]
-        x2 = x[:,512:768,:,:]
-        response = torch.cat([response, x1], dim=1)
-        response = self.update2(response)
-        response = torch.cat([response, x0], dim=1)
-        response = self.update3(response)
-        # response += x0
+        # x1 = x[:,256:512,:,:]
+        # x2 = x[:,512:768,:,:]
+        # response = torch.cat([response, x1], dim=1)
+        # response = self.update2(response)
+        # response = torch.cat([response, x0], dim=1)
+        # response = self.update3(response)
+        response += x0
         return response
